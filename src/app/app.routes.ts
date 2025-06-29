69dba36d8670e3a41c9017afe06d20f8c99fe2c9
EB <be_eden2000@yahoo.com>
2025-06-28 22:02:53 -0700

    adding whole project

---
+++ src/app/app.routes.ts
@@ -0,0 +1,14 @@
+import { Routes } from '@angular/router';
+import { MapComponent } from './map/map.component';
+
+export const routes: Routes = [
+  {
+    path: 'world-map',
+    component: MapComponent
+  },
+  {
+    path: '',
+    redirectTo: '/world-map',
+    pathMatch: 'full'
+  }
+];
